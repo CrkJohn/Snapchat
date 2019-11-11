5428c07967100eefa8d70ef94927de924eadde63
CrkJohn <JhonhRge@gmail.com>
2019-11-11 11:25:44 -0500

    Funciono por fin

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -2,32 +2,63 @@ package tests;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileBrowserType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.*;
+import java.util.concurrent.TimeUnit;
+
 public class BaseClass {
 
     public static AppiumDriver<MobileElement> driver;
 
+    ReadProperties capProperties = new ReadProperties();
+
+    @Test
+    public void sampleTest(){
+        System.out.println("Rocha es marica");
+    }
+
     @BeforeTest
     public void setup() {
+
+
+
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"ANDROID");
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.0.1");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "ALE L23");
-        desiredCapabilities.setCapability(MobileCapabilityType.UDID, "G7T7N16B10000883");
-        desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 120);
-        desiredCapabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-        //desiredCapabilities.setCapability(MobileCapabilityType.APPLICATION_NAME,"CHROME");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "5.0.2");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "LG Magna LTE");
+        desiredCapabilities.setCapability(MobileCapabilityType.UDID, "4TIBNREMDAMFPB9L");
+        desiredCapabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 300);
+        desiredCapabilities.setCapability("takesScreenshot",true);
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.snapchat.android.LandingPageActivity");
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.snapchat.android");
+
+        desiredCapabilities.setCapability("app","C:\\Users\\user\\Documents\\Snapchat\\src\\test\\resources\\Snapchat.apk");
+
+
+
         try {
-            URL url = new URL("http://127.0.0.1:4723/wb/hub");
+            URL url = new URL("http://127.0.0.1:4723/wd/hub");
             driver = new AppiumDriver<MobileElement>(url,desiredCapabilities);
+            driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            driver.findElement(By.id("com.snapchat.android:id/login_and_signup_page_fragment_login_button")).click();
+            driver.findElement(By.id("com.snapchat.android:id/username_or_email_field")).setValue("tcontinuum");
+            driver.findElement(By.id("com.snapchat.android:id/password_field")).setValue("test12345");
+            driver.findElement(By.id("com.snapchat.android:id/button_text")).click();
+
+
+
         } catch (MalformedURLException e) {
             System.out.println("This cause is  " + e.getCause());
             System.out.println("This messages is  " + e.getMessage());
@@ -37,10 +68,7 @@ public class BaseClass {
 
     }
 
-    @Test
-    public void sampleTest(){
-            System.out.println("Rocha es marica");
-    }
+
 
     @AfterTest
     public void tearDown(){
---
+++ src/test/java/tests/ReadProperties.java
@@ -0,0 +1,27 @@
+package tests;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperties {
+
+    private Properties property;
+
+
+    public void initProperty(String propFileName) {
+        try {
+            FileInputStream fis;
+            this.property = new Properties();
+            fis = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\" + propFileName);
+            property.load(fis);
+        } catch (IOException e) {
+            System.err.println("Error: properties file is absent");
+        }
+    }
+
+
+    public String getPropValue(String propName) {
+        return property.getProperty(propName);
+    }
+}
