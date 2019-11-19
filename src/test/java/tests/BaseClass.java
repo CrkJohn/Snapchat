0c1efb5b75ca6e7d40ad1d324013ce207d9e2a61
CrkJohn <JhonhRge@gmail.com>
2019-11-18 23:04:04 -0500

    Server and chat

--- src/test/java/tests/BaseClass.java
+++ src/test/java/tests/BaseClass.java
@@ -8,6 +8,15 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileBrowserType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.internal.Coordinates;
@@ -20,7 +29,11 @@ import org.testng.annotations.Test;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -59,23 +72,57 @@ public class BaseClass {
            /* for (char c: psw.toCharArray()) {
                 Thread.sleep(1);
             }*/
-            driver.findElement(By.id("com.snapchat.android:id/username_or_email_field")).sendKeys(user);
+            driver.findElement(By.id("com.snapchat.android:id/username_or_email_field")).setValue(
+                    user);
             driver.findElement(By.id("com.snapchat.android:id/password_field")).setValue(psw);
             driver.findElement(By.id("com.snapchat.android:id/button_text")).click();
             driver.findElement(By.id("com.snapchat.android:id/feed_icon_container")).click();
             Thread.sleep(30000);
             List<MobileElement> ele  = driver.findElements(By.className("android.view.View"));
             ele.get(0).click();
+            Thread.sleep(20000);
+            List<MobileElement> mensajes  = driver.findElements(By.xpath("//androidx.recyclerview.widget.RecyclerView/*"));
+            System.out.println(mensajes.size());
+            for(MobileElement me : mensajes){
+                System.out.println(" text  " +me.getText() + "  tagName " + me.getTagName()  + " string "  + me.toString());
+            }
+
+
             driver.findElement(By.id("com.snapchat.android:id/chat_input_text_field")).sendKeys("esto es un bot");
             Robot r = new Robot();
             r.keyPress(KeyEvent.VK_ENTER);
             r.keyRelease(KeyEvent.VK_ENTER);
-            System.out.println(ele.size());
 
-        } catch (MalformedURLException | InterruptedException | AWTException e) {
+            /*
+             HTTP POST
+             */
+
+            HttpClient httpclient = HttpClients.createDefault();
+            HttpPost httppost = new HttpPost("http://localhost:5000");
+
+            // Request parameters and other properties.
+            List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+            params.add(new BasicNameValuePair("kha", "12345"));
+            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+            //Execute and get the response.
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null) {
+                try (InputStream instream = entity.getContent()) {
+                    // do something useful
+                }
+            }
+            System.out.println(ele.size() );
+
+        } catch (MalformedURLException | InterruptedException | AWTException | UnsupportedEncodingException e) {
             System.out.println("This cause is  " + e.getCause());
             System.out.println("This messages is  " + e.getMessage());
 
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
